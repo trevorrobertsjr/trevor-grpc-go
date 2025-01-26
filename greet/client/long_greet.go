75bf79c117a4b25c0110fc905dfadd9245e084ab
Trevor Roberts Jr <trevor.roberts.jr@gmail.com>
2025-01-26 16:34:22 -0500

    complete client streaming example

---
+++ greet/client/long_greet.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/trevorrobertsjr/trevor-grpc-go/greet/proto"
+)
+
+func doLongGreet(c pb.GreetServiceClient) {
+	log.Println("doLongGreet was invoked")
+
+	reqs := []*pb.GreetRequest{
+		{FirstName: "Trevor"},
+		{FirstName: "Isla"},
+		{FirstName: "Eric"},
+	}
+	stream, err := c.LongGreet(context.Background())
+
+	if err != nil {
+		log.Fatalf("Error while calling LongGreet %v\n", err)
+	}
+
+	for _, req := range reqs {
+		log.Printf("Sending a req: %v", req)
+		stream.Send(req)
+		time.Sleep(1 * time.Second)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error while receiving response from LongGreet: %v\n", err)
+	}
+
+	log.Printf("LongGreet: %s\n", res.Result)
+}
