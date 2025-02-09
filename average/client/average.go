ce918bdc2b618d6bcda67a7ba5e8d555d6247b3e
Trevor Roberts Jr <trevor.roberts.jr@gmail.com>
2025-02-09 08:15:03 -0500

    updated average client to accept a list of numbers when it is executed. doAverage converts that list to Number messages

--- average/client/average.go
+++ average/client/average.go
@@ -7,25 +7,22 @@ import (
 	pb "github.com/trevorrobertsjr/trevor-grpc-go/average/proto"
 )
 
-func doAverage(c pb.AverageServiceClient) {
+func doAverage(c pb.AverageServiceClient, numbers []int32) {
 	log.Println("doAverage was invoked")
 
-	reqs := []*pb.AverageRequest{
-		// {Number: 3},
-		// {Number: 4},
-		// {Number: 3},
-	}
 	stream, err := c.Average(context.Background())
-
 	if err != nil {
-		log.Fatalf("Error while calling Average %v\n", err)
+		log.Fatalf("Error while calling Average: %v\n", err)
 	}
 
-	for _, req := range reqs {
-		log.Printf("Sending a req: %v\n", req)
+	// Convert numbers to gRPC request messages and send them
+	for _, num := range numbers {
+		req := &pb.AverageRequest{Number: num}
+		log.Printf("Sending request: %v\n", req)
 		stream.Send(req)
 	}
 
+	// Receive and print the response
 	res, err := stream.CloseAndRecv()
 	if err != nil {
 		log.Fatalf("Error while receiving response from Average: %v\n", err)
--- average/client/main.go
+++ average/client/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"os"
+	"strconv"
 
 	pb "github.com/trevorrobertsjr/trevor-grpc-go/average/proto"
 	"google.golang.org/grpc"
@@ -11,14 +13,26 @@ import (
 var addr string = "localhost:50051"
 
 func main() {
-	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// Parse numbers from command-line arguments
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: %s <num1> <num2> ... <numN>\n", os.Args[0])
+	}
 
+	var numbers []int32
+	for _, arg := range os.Args[1:] {
+		num, err := strconv.Atoi(arg)
+		if err != nil {
+			log.Fatalf("Invalid number %s: %v", arg, err)
+		}
+		numbers = append(numbers, int32(num))
+	}
+
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("Failed to connect %v\n", err)
+		log.Fatalf("Failed to connect: %v\n", err)
 	}
 	defer conn.Close()
+
 	c := pb.NewAverageServiceClient(conn)
-
-	doAverage(c)
-
+	doAverage(c, numbers)
 }
